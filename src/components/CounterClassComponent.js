47399c65d8e85fb2d9c3ed8d378aaeaee6d1d3c4
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-04-29 11:12:35 +0530

    sonarQube test passed

---
+++ src/components/CounterClassComponent.js
@@ -0,0 +1,51 @@
+
+
+
+import { Component } from "react";
+
+import { connect } from "react-redux";
+
+
+ 
+class Counter1 extends Component {
+  incrementHandler() {
+    this.props.increment();
+  }
+
+  decrementHandler() {
+    this.props.decrement();
+  }
+
+  toggleCounterHandler() {}
+
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.incrementHandler}> Increment</button>
+          <button onClick={this.decrementHandler}> Decrement</button>
+        </div>
+
+        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: "increment" }),
+    decrement: () => dispatch({ type: "decrement" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter1);
+
+ 
---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import { createStore } from "redux";
+
+const initialState = {
+  counter: 0,
+  showCounter: true,
+};
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+      // showCounter: true,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      ...state,
+      counter: state.counter + action.amount,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      ...state,
+      counter: state.counter - 1,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
+    };
+  }
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
